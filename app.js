6cac475b1824878bdf08d2c34506ee657dfa757c
Daniel Kro <85504684+Danielkro96@users.noreply.github.com>
2023-01-20 22:07:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //Requiring mailchimp's module
-//For this we need to install the npm module @mailchimp/mailchimp_marketing. To do that we write:
-//npm install @mailchimp/mailchimp_marketing
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 //Requiring express and body parser and initializing the constant "app"
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
+
 //Using bod-parser
 app.use(bodyParser.urlencoded({extended:true}));
-//The public folder which holds the CSS
+//The public folder that hold CSS
 app.use(express.static("public"));
 //Listening on port 3000 and if it goes well then logging a message saying that the server is running
 app.listen(process.env.PORT||3000,function () {
@@ -18,27 +18,29 @@ app.listen(process.env.PORT||3000,function () {
 app.get("/", function (req, res) {
  res.sendFile(__dirname + "/signup.html");
 });
+
 //Setting up MailChimp
 mailchimp.setConfig({
-//*****************************ENTER YOUR API KEY HERE******************************
+//***************************** API KEY ******************************
  apiKey: "07bec897449675102ab2b15e0fc36de7-us6",
-//*****************************ENTER YOUR API KEY PREFIX HERE i.e.THE SERVER******************************
+//***************************** API KEY PREFIX (THE SERVER) ******************************
  server: "us6"
 });
-//As soon as the sign in button is pressed execute this
+
+//As the sign in button is pressed execute this
 app.post("/", function (req,res) {
-//*****************************CHANGE THIS ACCORDING TO THE VALUES YOU HAVE ENTERED IN THE INPUT ATTRIBUTE IN HTML******************************
 const firstName = req.body.firstName;
 const secondName = req.body.lastName;
 const email = req.body.email;
-//*****************************ENTER YOU LIST ID HERE******************************
+//***************************** LIST ID HERE ******************************
 const listId = "24be80779a";
-//Creating an object with the users data
+//Creating an object with the user info
 const subscribingUser = {
  firstName: firstName,
  lastName: secondName,
  email: email
 };
+ 
 //Uploading the data to the server
 async function run() {
   const response = await mailchimp.lists.addListMember(listId, {
@@ -54,7 +56,6 @@ async function run() {
    console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}.`);
  }
 //Running the function and catching the errors (if any)
-// ************************THIS IS THE CODE THAT NEEDS TO BE ADDED FOR THE NEXT LECTURE*************************
-// So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
+//So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
  run().catch(e => res.sendFile(__dirname + "/failure.html"));
 });
